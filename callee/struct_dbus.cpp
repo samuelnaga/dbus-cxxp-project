2e78dd73861010be778a9c546839418eec7248cd
Samuel Navarro García <samuelnaga@hotmail.com>
2019-07-31 09:40:16 +0200

    fixed operators ?

--- callee/struct_dbus.cpp
+++ callee/struct_dbus.cpp
@@ -3,7 +3,6 @@
 
 DBus::MessageIterator& operator>>(DBus::MessageIterator& i, struct sBlock& t)
 {
-
     DBus::MessageIterator subiter = i.recurse();
     subiter >> t.data;
     subiter >> t.block;
@@ -14,12 +13,14 @@ DBus::MessageIterator& operator>>(DBus::MessageIterator& i, struct sBlock& t)
 
 DBus::MessageAppendIterator& operator<<(DBus::MessageAppendIterator& i, const struct sBlock& t)
 {
-    i.open_container(DBus::CONTAINER_STRUCT, std::string());
-    i.sub_iterator()->append(t.data);
-    i.sub_iterator()->append(t.block);
-    i.close_container();
-
+    i.append(t.block);
+    i.append(t.data);
     return i;
+
+//    i.open_container(DBus::CONTAINER_STRUCT, std::string());
+//    i.sub_iterator()->append(t.data);
+//    i.sub_iterator()->append(t.block);
+//    i.close_container();
 }
 
 
@@ -29,18 +30,20 @@ DBus::MessageIterator& operator>>(DBus::MessageIterator& i, struct sResult& t)
     DBus::MessageIterator subiter = i.recurse();
     subiter >> t.res;
     subiter >> t.blocks;
-
+    //t.blocks = i.get_array_complex<sBlock>();
     return i;
 }
 
 
 DBus::MessageAppendIterator& operator<<(DBus::MessageAppendIterator& i, const struct sResult& t)
 {
-    i.open_container(DBus::CONTAINER_STRUCT, std::string());
-    i.sub_iterator()->append(t.res);
-    i.sub_iterator()->append(t.blocks);
-    i.close_container();
+//    i.open_container(DBus::CONTAINER_STRUCT, std::string());
+//    i.sub_iterator()->append(t.res);
+//    i.sub_iterator()->append(t.blocks);
+//    i.close_container();
 
+    i.append(t.res);
+    i.append(t.blocks);
     return i;
 }
 
