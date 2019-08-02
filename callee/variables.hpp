3fc204f6b4ea720f10073acd603f6d5090f31eb9
Samuel Navarro García <samuelnaga@hotmail.com>
2019-08-02 12:26:06 +0200

    fixed signatures
    added bock array signature

--- callee/variables.hpp
+++ callee/variables.hpp
@@ -5,14 +5,16 @@
 
 
 struct sBlock{
-    static constexpr const char* DBUS_SIGNATURE = "(ya)";
+    static constexpr const char* BLOCK_SIGNATURE = "(yay)";
     uint8_t block;
     std::vector<uint8_t> data;
 };
 
 
 struct sResult {
-    static constexpr const char* DBUS_SIGNATURE = "(ya)";
+    static constexpr const char* RESULT_SIGNATURE = "(ya(yay))";
     uint8_t res;
     std::vector<sBlock> blocks;
 };
+
+static constexpr const char* BLOCK_ARRAY_SIGNATURE = "(a(yay))";
