dcff0addbd7861ab60015fca382f8509d02f81d5
Samuel Navarro García <samuelnaga@hotmail.com>
2019-08-08 10:10:14 +0200

    sblock array signatures

--- caller/struct_dbus.cpp
+++ caller/struct_dbus.cpp
@@ -39,3 +39,32 @@ DBus::MessageAppendIterator& operator<<(DBus::MessageAppendIterator& i, const st
 }
 
 
+DBus::MessageIterator& operator>>(DBus::MessageIterator& i, std::vector<sBlock>& t)
+{
+    DBus::MessageIterator subiter = i.recurse();
+    for (size_t j=0; j < t.size(); ++j)
+    {
+        DBus::MessageIterator subiter2 = subiter.recurse();
+        subiter2 >> t[j].data;
+        subiter2 >> t[j].block;
+    }
+    return i;
+}
+
+
+DBus::MessageAppendIterator& operator<<(DBus::MessageAppendIterator& i, const std::vector<sBlock>& t)
+{
+    i.open_container(DBus::CONTAINER_ARRAY, std::string());
+
+    for (size_t j=0; j < t.size(); ++j)
+    {
+        i.sub_iterator()->open_container(DBus::CONTAINER_STRUCT, std::string());
+        i.sub_iterator()->sub_iterator()->append(t[j].data);
+        i.sub_iterator()->sub_iterator()->append(t[j].block);
+        i.sub_iterator()->close_container();
+    }
+
+    i.close_container();
+    return i;
+}
+
