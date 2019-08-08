4cadcabb33d01c906628b7a0ad22f799026d5405
Samuel Navarro García <samuelnaga@hotmail.com>
2019-08-08 10:11:55 +0200

    block signatures

--- callee/struct_dbus.hpp
+++ callee/struct_dbus.hpp
@@ -4,15 +4,17 @@
 #include <dbus-cxx/types.h>
 
 namespace DBus {
-  inline std::string signature(struct sBlock){ return sBlock::BLOCK_SIGNATURE; }
-  inline std::string signature(struct sResult ){ return sResult::RESULT_SIGNATURE; }
-  // inline std::string signature(std::vector<sBlock>) { return BLOCK_ARRAY_SIGNATURE; }
+    inline std::string signature(struct sBlock){ return sBlock::BLOCK_SIGNATURE; }
+    inline std::string signature(struct sResult ){ return sResult::RESULT_SIGNATURE; }
+    inline std::string signature(std::vector<sBlock>) { return BLOCK_ARRAY_SIGNATURE; }
 
-  inline std::string type_string(const struct sBlock&) { return "Struct"; }
-  inline std::string type_string(const struct sResult&)    { return "Struct"; }
+    inline std::string type_string(const struct sBlock&) { return "Struct"; }
+    inline std::string type_string(const struct sResult&)    { return "Struct"; }
+    inline std::string type_string(const std::vector<sBlock>&)    { return "Array"; }
 
-  inline DBus::Type type(const struct sBlock&) { return DBus::TYPE_STRUCT; }
-  inline DBus::Type type(const struct sResult&)    { return DBus::TYPE_STRUCT; }
+    inline DBus::Type type(const struct sBlock&) { return DBus::TYPE_STRUCT; }
+    inline DBus::Type type(const struct sResult&)    { return DBus::TYPE_STRUCT; }
+    inline DBus::Type type(const std::vector<sBlock>&)    { return DBus::TYPE_ARRAY; }
 }
 
 #include <dbus-cxx.h>
@@ -22,3 +24,6 @@ DBus::MessageAppendIterator& operator<<(DBus::MessageAppendIterator& i, const st
 
 DBus::MessageIterator& operator>>(DBus::MessageIterator& i, struct sResult& t);
 DBus::MessageAppendIterator& operator<<(DBus::MessageAppendIterator& i, const struct sResult& t);
+
+DBus::MessageIterator& operator>>(DBus::MessageIterator& i, std::vector<sBlock>& t);
+DBus::MessageAppendIterator& operator<<(DBus::MessageAppendIterator& i, const std::vector<sBlock>& t);
